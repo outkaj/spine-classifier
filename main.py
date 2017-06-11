cdec35489f627d1a73b9c36af30f58a473596067
Jacqueline Outka <jacqueline@outka.xyz>
2017-06-11 03:13:19 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,119 @@
+#!/usr/bin/python
+
+import sys
+import os
+from PIL import Image
+import pyocr
+import pyocr.builders
+import cv2
+import numpy as np
+
+tools = pyocr.get_available_tools()
+if len(tools) == 0:
+    print("No OCR tool found")
+    sys.exit(1)
+# The tools are returned in the recommended order of usage
+tool = tools[0]
+print("       Will use tool '%s'" % (tool.get_name()))
+# Ex: Will use tool 'libtesseract'
+
+langs = tool.get_available_languages()
+print("       Available languages: %s" % ", ".join(langs))
+lang = langs[1]
+print("       Will use lang '%s'" % (lang))
+# Ex: Will use lang 'fra'
+# Note that languages are NOT sorted in any way. Please refer
+# to the system locale settings for the default language
+# to use.
+
+print('       A demo script of the Extremal Region Filter algorithm described in:')
+print('       Neumann L., Matas J.: Real-Time Scene Text Localization and Recognition, CVPR 2012\n')
+if (len(sys.argv) < 2):
+  print(' (ERROR) You must call this script with an argument (path_to_image_to_be_processed)\n')
+  quit()
+
+pathname = os.path.dirname(sys.argv[0])
+
+
+img      = cv2.imread(str(sys.argv[1]))
+# for visualization
+vis      = img.copy()
+
+
+# Extract channels to be processed individually
+channels = cv2.text.computeNMChannels(img)
+# Append negative channels to detect ER- (bright regions over dark background)
+cn = len(channels)-1
+for c in range(0,cn):
+  channels.append((255-channels[c]))
+
+# Apply the default cascade classifier to each independent channel (could be done in parallel)
+print("Extracting Class Specific Extremal Regions from "+str(len(channels))+" channels ...")
+print("    (...) this may take a while (...)")
+for channel in channels:
+
+  erc1 = cv2.text.loadClassifierNM1('trained_classifierNM1.xml')
+  er1 = cv2.text.createERFilterNM1(erc1,16,0.00015,0.13,0.2,True,0.1)
+
+  erc2 = cv2.text.loadClassifierNM2('trained_classifierNM2.xml')
+  er2 = cv2.text.createERFilterNM2(erc2,0.5)
+
+  regions = cv2.text.detectRegions(channel,er1,er2)
+
+  rects = cv2.text.erGrouping(img,channel,[r.tolist() for r in regions])
+  #rects = cv2.text.erGrouping(img,channel,[x.tolist() for x in regions], cv2.text.ERGROUPING_ORIENTATION_ANY,'../../GSoC2014/opencv_contrib/modules/text/samples/trained_classifier_erGrouping.xml',0.5)
+
+  #Visualization
+  for r in range(0,np.shape(rects)[0]):
+    rect = rects[r]
+    cv2.rectangle(vis, (rect[0],rect[1]), (rect[0]+rect[2],rect[1]+rect[3]), (0, 0, 0), 2)
+    cv2.rectangle(vis, (rect[0],rect[1]), (rect[0]+rect[2],rect[1]+rect[3]), (255, 255, 255), 1)
+
+
+#Visualization
+cv2.imshow("Text detection result", vis)
+cv2.waitKey(0)
+
+txt = tool.image_to_string(
+    Image.open(vis),
+    lang=lang,
+    builder=pyocr.builders.TextBuilder()
+)
+# txt is a Python string
+
+word_boxes = tool.image_to_string(
+    Image.open(vis),
+    lang="eng",
+    builder=pyocr.builders.WordBoxBuilder()
+)
+# list of box objects. For each box object:
+#   box.content is the word in the box
+#   box.position is its position on the page (in pixels)
+#
+# Beware that some OCR tools (Tesseract for instance)
+# may return empty boxes
+
+line_and_word_boxes = tool.image_to_string(
+    Image.open(vis), lang="eng",
+    builder=pyocr.builders.LineBoxBuilder()
+)
+# list of line objects. For each line object:
+#   line.word_boxes is a list of word boxes (the individual words in the line)
+#   line.content is the whole text of the line
+#   line.position is the position of the whole line on the page (in pixels)
+#
+# Beware that some OCR tools (Tesseract for instance)
+# may return empty boxes
+
+# Digits - Only Tesseract (not 'libtesseract' yet !)
+digits = tool.image_to_string(
+    Image.open('test-digits.png'),
+    lang=lang,
+    builder=pyocr.tesseract.DigitBuilder()
+)
+# digits is a python string
+
+print(txt)
+print(word_boxes)
+print(line_and_word_boxes)
+print(digits)
