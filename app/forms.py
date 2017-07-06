71cea4458c642866c718daa26b47f8bef6f0aa3b
Jacqueline Outka <jacqueline@outka.xyz>
2017-07-05 21:14:52 -0400

    Attempt to remove duplicate files

--- app/forms.py
+++
@@ -1,8 +0,0 @@
-# ourapp/forms.py
-
-from flask_wtf import Form
-from wtforms import StringField, PasswordField
-from wtforms.validators import DataRequired, Email
-
-class FilepathForm(Form):
-    filepath = StringField('Filepath', validators=[DataRequired(), Filepath()])
